735d983c8009caab2f66d76c488d6d249827d6c8
柿内暢昌 <nob@studio-kakky.com>
2015-08-15 13:53:30 +0900

    資料完了

--- gulpfile.js
+++ gulpfile.js
@@ -12,13 +12,27 @@ gulp.task('copy:reveal', function() {
   .pipe(gulp.dest(DESTDIR+'lib/reveal'));
 });
 
+gulp.task('copy:snapsvg', function() {
+  return gulp.src([
+    'bower_components/snap.svg/dist/snap.svg-min.js'
+  ])
+  .pipe(gulp.dest('demo/script/extlib/'));
+});
+
+
 gulp.task('copy:html', function() {
   return gulp.src('presentation/src/**/*.*')
     .pipe(gulp.dest(DESTDIR));
 });
 
-gulp.task('watch', function() {
-  gulp.watch('presentation/src/**/*.*',['copy:html']);
+gulp.task('copy:demo', function() {
+  return gulp.src('demo/**/*.*',{base:'./'})
+    .pipe(gulp.dest(DESTDIR));
 });
 
-gulp.task('default',['copy:reveal','copy:html','watch']);
+gulp.task('watch', function() {
+  gulp.watch('presentation/src/**/*.*',['copy:html']);
+  gulp.watch('demo/**/*.*',['copy:demo']);
+});
+
+gulp.task('default',['copy:reveal','copy:html','copy:demo','watch']);
